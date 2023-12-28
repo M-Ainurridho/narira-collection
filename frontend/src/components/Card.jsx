f3c0561b0c9dcbd79e0fbb5d51a5a54e5e2c6010
Muhammad Ainurridho <m.ainurridho11@gmail.com>
2023-12-28 22:32:00 +0700

    update card image

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -24,7 +24,7 @@ const Card = ({ image, children }) => {
          )}
          <div className="card-img">
             <img
-               src={`https://narira-collection.vercel.app/src/assets/images/products/${image}`}
+               src={`/src/assets/images/products/${image}`}
                className="w-full h-56 object-cover object-center"
             />
          </div>
